b3230542cf27c908893a8b9c0784d12bb4fb3450
Māris Samats <maris.samats@gmail.com>
2021-12-27 00:18:13 +0200

    Added more elaborate input components

--- simulator-ts-threejs-react/src/Components/Controls.tsx
+++ simulator-ts-threejs-react/src/Components/Controls.tsx
@@ -6,8 +6,49 @@ export const Controls: React.FC = () => {
     if (!frames) {
         return <UploadControls />;
     }
-    return <div></div>;
+    return <UploadControls />;
 };
 export const UploadControls: React.FC = () => {
-    return <div></div>;
+    return (
+        <div>
+            <FromUrl />
+            <span>or</span>
+            <FromFile />
+        </div>
+    );
+};
+const FormError: React.FC<{ text: string }> = ({ text }) => {
+    if (!text) {
+        return null;
+    }
+    return <p style={{ color: "red" }}>{text}</p>;
+};
+const FromUrl = () => {
+    const onSubmit = React.useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }, []);
+    return (
+        <form onSubmit={onSubmit}>
+            <FormError text="asd" />
+            <input type="text" placeholder="Enter URL" />
+            <input type="submit" />
+        </form>
+    );
+};
+
+const FromFile = () => {
+    const onSubmit = React.useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }, []);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            console.log(e.target.files);
+        }
+    }, []);
+    return (
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="file" placeholder="Upload" accept=".csv" multiple={false} />
+            <input type="submit" />
+        </form>
+    );
 };
